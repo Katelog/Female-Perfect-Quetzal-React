f7bfaa0fcbb9c8847d3bd1b04ff6840e0f8796c1
Katelog <167889591+Katelog@users.noreply.github.com>
2024-04-23 17:43:16 +0300

    Commit from TeleportHQ

---
+++ src/views/frame.js
@@ -0,0 +1,425 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './frame.css'
+
+const Frame = (props) => {
+  return (
+    <div className="frame-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="frame-frame">
+        <div className="frame-group1001">
+          <div className="frame-group2">
+            <div>
+              <span className="frame-text">
+                <span>Помощь</span>
+              </span>
+            </div>
+          </div>
+          <div className="frame-group3">
+            <div className="frame-frame11">
+              <span className="frame-text02">
+                <span>Выйти</span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="frame-group1007">
+          <div className="frame-group1006">
+            <img
+              alt="Rectangle32119"
+              src="/external/rectangle32119-kbk7-300h.png"
+              className="frame-rectangle3"
+            />
+            <img
+              alt="Union2119"
+              src="/external/union2119-xyok.svg"
+              className="frame-union"
+            />
+            <span className="frame-text04">
+              <span>Выдать стакан</span>
+              <br></br>
+              <span>клиенту</span>
+            </span>
+            <img
+              alt="Rectangle72119"
+              src="/external/rectangle72119-9lr-300h.png"
+              className="frame-rectangle7"
+            />
+            <img
+              alt="Union2119"
+              src="/external/union2119-wli.svg"
+              className="frame-union1"
+            />
+            <span className="frame-text08">
+              <span>Принять стакан</span>
+              <br></br>
+              <span>обратно</span>
+            </span>
+          </div>
+          <div className="frame-group1005">
+            <span className="frame-text12">
+              <span>Шоколадница</span>
+            </span>
+            <span className="frame-text14">
+              <span>Москва, ул. Ленина, 32</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="frame-landing">
+        <div className="frame-status-bar">
+          <div className="frame-notch">
+            <img
+              alt="NotchI211"
+              src="/external/notchi211-y55j.svg"
+              className="frame-notch1"
+            />
+          </div>
+          <div className="frame-left-side">
+            <div className="frame-frame-status-bartime">
+              <span className="frame-text16">
+                <span>9:41</span>
+              </span>
+            </div>
+          </div>
+          <div className="frame-right-side">
+            <img
+              alt="SVGStatusBarbatteryI211"
+              src="/external/svgstatusbarbatteryi211-4t1m.svg"
+              className="frame-svg-status-barbattery"
+            />
+            <img
+              alt="WifiI211"
+              src="/external/wifii211-ghf.svg"
+              className="frame-wifi"
+            />
+            <img
+              alt="IconMobileSignalI211"
+              src="/external/iconmobilesignali211-fo9q.svg"
+              className="frame-icon-mobile-signal"
+            />
+          </div>
+        </div>
+        <div className="frame-header">
+          <div className="frame-frame-header-left-content">
+            <img
+              alt="IconI211"
+              src="/external/iconi211-78ew.svg"
+              className="frame-icon"
+            />
+          </div>
+          <div className="frame-frame-header-central-content">
+            <div className="frame-frame-title">
+              <span className="frame-text18 EcoplatformHeadlineMediumM—20px">
+                <span>Бутылки и банки</span>
+              </span>
+            </div>
+          </div>
+          <div className="frame-frame-header-right-content"></div>
+        </div>
+        <div className="frame-content">
+          <div>
+            <div className="frame-pic">
+              <div className="frame-bg"></div>
+              <div className="frame-fando">
+                <img
+                  alt="palindromecore3dalumuniumcan966e1144584542eb90b5592117"
+                  src="/external/palindromecore3dalumuniumcan966e1144584542eb90b5592117-9wp-200w.png"
+                  className="frame-palindromecore3dalumuniumcan966e1144584542eb90b559"
+                />
+                <img
+                  alt="imageremovebgpreview1112117"
+                  src="/external/imageremovebgpreview1112117-a4vm-300h.png"
+                  className="frame-imageremovebgpreview111"
+                />
+              </div>
+              <img
+                alt="IMAGE26442117"
+                src="/external/image26442117-b9ce-200h.png"
+                className="frame-image2644"
+              />
+            </div>
+            <span className="frame-text20 EcoplatformBodyRegularXS—14px">
+              <span>
+                Сдавайте пластиковые бутылки и алюминиевые банки на переработку
+                в фандоматы и получайте баллы за ваш вклад в сохранение
+                окружающей среды в бонусной программе Ecoplatform.
+              </span>
+            </span>
+          </div>
+          <div className="frame-block2">
+            <span className="frame-text22 EcoplatformHeadlineMediumM—20px">
+              <span>Что принимает фандомат?</span>
+            </span>
+            <div className="frame-block">
+              <div className="frame-frame1 frame-frame1">
+                <div className="frame-pic1">
+                  <img
+                    alt="palindromecore3dalumuniumcan966e1144584542eb90b5592117"
+                    src="/external/palindromecore3dalumuniumcan966e1144584542eb90b5592117-ph2j-200w.png"
+                    className="frame-palindromecore3dalumuniumcan966e1144584542eb90b5591"
+                  />
+                  <img
+                    alt="IMAGE26442117"
+                    src="/external/image26442117-4t2-200w.png"
+                    className="frame-image26441"
+                  />
+                </div>
+                <div className="frame-content1">
+                  <span className="frame-text24 EcoplatformCaption—13px">
+                    <span>
+                      Пластиковые бутылки и алюминиевые банки
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                  </span>
+                </div>
+              </div>
+              <div className="frame-frame2">
+                <div className="frame-pic2">
+                  <img
+                    alt="IMAGE26442117"
+                    src="/external/image26442117-15rh-200w.png"
+                    className="frame-image26442"
+                  />
+                  <img
+                    alt="palindromecore3dalumuniumcan966e1144584542eb90b5592117"
+                    src="/external/palindromecore3dalumuniumcan966e1144584542eb90b5592117-hfp-200w.png"
+                    className="frame-palindromecore3dalumuniumcan966e1144584542eb90b5592"
+                  />
+                </div>
+                <div className="frame-content2">
+                  <span className="frame-text26 EcoplatformCaption—13px">
+                    <span>Без жидкости</span>
+                  </span>
+                </div>
+              </div>
+              <div className="frame-frame3">
+                <div className="frame-pic3">
+                  <img
+                    alt="IMAGE26442117"
+                    src="/external/image26442117-ffm-200w.png"
+                    className="frame-image26443"
+                  />
+                </div>
+                <div className="frame-content3">
+                  <span className="frame-text28 EcoplatformCaption—13px">
+                    <span>Объемом до 2 литров</span>
+                  </span>
+                </div>
+              </div>
+              <div className="frame-frame4">
+                <div className="frame-pic4">
+                  <img
+                    alt="IMAGE26442117"
+                    src="/external/image26442117-m8z-200w.png"
+                    className="frame-image26444"
+                  />
+                  <img
+                    alt="Rectangle13742117"
+                    src="/external/rectangle13742117-hvld-200w.png"
+                    className="frame-rectangle1374"
+                  />
+                  <img
+                    alt="Line62117"
+                    src="/external/line62117-1zpb.svg"
+                    className="frame-line6"
+                  />
+                  <img
+                    alt="Line72117"
+                    src="/external/line72117-500u.svg"
+                    className="frame-line7"
+                  />
+                  <img
+                    alt="Line82117"
+                    src="/external/line82117-z21e.svg"
+                    className="frame-line8"
+                  />
+                  <img
+                    alt="Line92117"
+                    src="/external/line92117-7ql.svg"
+                    className="frame-line9"
+                  />
+                  <img
+                    alt="Line102117"
+                    src="/external/line102117-vp2o.svg"
+                    className="frame-line10"
+                  />
+                </div>
+                <div className="frame-content4">
+                  <span className="frame-text30 EcoplatformCaption—13px">
+                    <span>Рекомендуем оставить штрихкод</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="frame-banner">
+            <div className="frame-banner1">
+              <span className="frame-text32 EcoplatformBodyMediumS—16px">
+                <span>Где найти фандоматы?</span>
+              </span>
+              <button className="frame-button">
+                <span className="frame-text34 EcoplatformBodyBoldXS—14px">
+                  <span>Открыть карту</span>
+                </span>
+              </button>
+              <img
+                alt="imageremovebgpreview312117"
+                src="/external/imageremovebgpreview312117-azr6-200w.png"
+                className="frame-imageremovebgpreview31"
+              />
+            </div>
+          </div>
+          <div className="frame-block1 frame-block1">
+            <div className="frame-content5">
+              <span className="frame-text36 EcoplatformHeadlineMediumM—20px">
+                <span>
+                  Как участвовать
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <br></br>
+                <span>в бонусной программе?</span>
+              </span>
+              <div className="frame-content6">
+                <div className="frame-content7">
+                  <div className="frame-pic5">
+                    <img
+                      alt="Vector312117"
+                      src="/external/vector312117-oe4k.svg"
+                      className="frame-vector31"
+                    />
+                    <img
+                      alt="Vector162117"
+                      src="/external/vector162117-6gcq.svg"
+                      className="frame-vector16"
+                    />
+                    <img
+                      alt="Vector172117"
+                      src="/external/vector172117-4ock.svg"
+                      className="frame-vector17"
+                    />
+                    <img
+                      alt="Vector182117"
+                      src="/external/vector182117-m3uk.svg"
+                      className="frame-vector18"
+                    />
+                    <img
+                      alt="Vector192117"
+                      src="/external/vector192117-n52g.svg"
+                      className="frame-vector19"
+                    />
+                    <img
+                      alt="Vector202117"
+                      src="/external/vector202117-0pe5.svg"
+                      className="frame-vector20"
+                    />
+                    <img
+                      alt="Vector212117"
+                      src="/external/vector212117-uwtc.svg"
+                      className="frame-vector21"
+                    />
+                    <img
+                      alt="Vector222117"
+                      src="/external/vector222117-rs0j.svg"
+                      className="frame-vector22"
+                    />
+                    <img
+                      alt="Vector232117"
+                      src="/external/vector232117-m7gn.svg"
+                      className="frame-vector23"
+                    />
+                    <img
+                      alt="Vector252117"
+                      src="/external/vector252117-4p2p.svg"
+                      className="frame-vector25"
+                    />
+                    <img
+                      alt="Vector242117"
+                      src="/external/vector242117-v88n.svg"
+                      className="frame-vector24"
+                    />
+                    <img
+                      alt="Vector302117"
+                      src="/external/vector302117-9u7.svg"
+                      className="frame-vector30"
+                    />
+                  </div>
+                  <span className="frame-text40 EcoplatformHeadlineMediumXL—32px">
+                    <span>02</span>
+                  </span>
+                </div>
+                <span className="frame-text42 EcoplatformBodyMediumS—16px">
+                  <span>Зарегистрировать  номер телефона</span>
+                </span>
+              </div>
+              <span className="frame-text44 EcoplatformBodyMediumXS—14px">
+                <span>
+                  при сдаче тары в фандомат или  в приложении. За каждую сданную
+                  бутылку или банку вам будет начислено 10 EcoБонусов.
+                </span>
+              </span>
+            </div>
+            <div className="frame-page-indicator">
+              <div className="frame-frame-page-indicator-dots-interactive">
+                <div className="frame-focus-group">
+                  <div className="frame-frame-page-indicator-item">
+                    <img
+                      alt="DotI211"
+                      src="/external/doti211-zepk-200h.png"
+                      className="frame-dot"
+                    />
+                  </div>
+                  <div className="frame-frame-page-indicator-item1">
+                    <img
+                      alt="DotI211"
+                      src="/external/doti211-l54-200h.png"
+                      className="frame-dot1"
+                    />
+                  </div>
+                  <div className="frame-frame-page-indicator-item2">
+                    <img
+                      alt="DotI211"
+                      src="/external/doti211-xah-200h.png"
+                      className="frame-dot2"
+                    />
+                  </div>
+                  <div className="frame-frame-page-indicator-item3">
+                    <img
+                      alt="DotI211"
+                      src="/external/doti211-6zy-200h.png"
+                      className="frame-dot3"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="frame-button1">
+            <img
+              alt="QRI211"
+              src="/external/qri211-agaq.svg"
+              className="frame-qr"
+            />
+            <span className="frame-text46 EcoplatformBodyBoldXS—14px">
+              <span>Сканировать QR</span>
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Frame
